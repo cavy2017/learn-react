7e6c714a228696f0d56ef98bc0fef595215f5544
Oksana <oksana.barnasheva@gmail.com>
2018-07-17 12:09:23 +0300

    react events

--- learn-react-app/src/App.js
+++ learn-react-app/src/App.js
@@ -3,11 +3,47 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephany', age: 26}
+    ]
+  }
+
+  changeNameHandler = (myName) => {
+    this.setState({persons: [
+      {name: myName, age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephany', age: 27}
+    ]});
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({persons: [
+      {name: 'Max', age: 28},
+      {name: event.target.value, age: 29},
+      {name: 'Stephany', age: 26}
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi! This is my react test sample</h1>
-        <Person />
+        <button onClick={() => this.changeNameHandler('M!!!')}>Change Name</button>
+        <Person 
+          name = {this.state.persons[0].name}
+          age = {this.state.persons[0].age}/>
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age}
+          click = {this.changeNameHandler.bind(this, 'MMM')}
+          changed = {this.onChangeHandler}
+          >Hobby: Racing</Person>
+        <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age}/>
       </div>
     );
   }
--- learn-react-app/src/Person/Person.js
+++ learn-react-app/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Hi! This is a Person block test</p>
+const person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>Hi! My name is {props.name}. I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} />
+        </div>
+        )
 };
 
 export default person;
