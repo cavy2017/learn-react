221bf235053fafa4ad6bb57fa48da3b434e8dc8e
Oksana <oksana.barnasheva@gmail.com>
2018-07-24 09:57:04 +0300

    Component life cycle demo

--- learn-react-app/src/containers/App.js
+++ learn-react-app/src/containers/App.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   state = {
     persons: [
       {id: 'sdf', name: 'Max', age: 28},
       {id: 'aaa', name: 'Manu', age: 29},
       {id: 'hhk', name: 'Stephany', age: 26}
     ]
+  }
+
+  componentWillMount () {
+    console.log('[App.js] inside componentWillMount()');
+  }
+
+  componentDidMount () {
+    console.log('[App.js] inside componentDidMount()');
+  }
+
+  // shouldComponentUpdate (nextProps, nextState) {
+  //   console.log('[UPDATE App.js] inside shouldComponentUpdate');
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  // }
+
+  componentWillUpdate (nextProps, nextState) {
+      console.log('[UPDATE App.js] Inside componentWillUpdate');
+  }
+
+  componentDidUpdate () {
+      console.log('[UPDATE App.js] Inside componentDidUpdate');
   }
 
   onChangeHandler = (event, id) => {
